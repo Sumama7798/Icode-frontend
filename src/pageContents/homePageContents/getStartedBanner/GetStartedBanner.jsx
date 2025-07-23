5954f56d64c2b413a664f62ff117150a77f89af1
Sumama7798 <sumamabutt500@gmail.com>
2025-07-23 20:09:43 +0500

    dsa

--- src/pageContents/homePageContents/getStartedBanner/GetStartedBanner.jsx
+++ src/pageContents/homePageContents/getStartedBanner/GetStartedBanner.jsx
@@ -8,7 +8,7 @@ const GetStartedBanner = () => {
     const navigate = useNavigate()
 
   return (
-    <div className=' bg-neutral-700 flex flex-col h-1/5 justify-center p-10 mx-auto'>
+    <div className=' bg-white flex flex-col h-1/5 justify-center p-10 mx-auto'>
 
         <motion.div
         variants={fadeIn('up', 0.3, 0.3)}
@@ -16,22 +16,61 @@ const GetStartedBanner = () => {
         whileInView="show"
         viewport={{ once: true, amount: 0.3 }}  
         className='flex sm:flex-row lg:flex-row flex-col m-auto text-4xl sm:text-6xl 
-        lg:text-6xl font-prompt font-semibold text-white items-center justify-center  
+        lg:text-6xl font-prompt font-semibold text-red-700 items-center justify-center  
         mx-auto mb-5'>          
-        <h1>Have An MVP Idea?</h1>        
+        <h1>We Are The Change Makers!</h1>        
         </motion.div>
 
         <div className='flex m-auto text-lg font-prompt justify-center items-center pt-5 mx-auto'>
           
-        <motion.button
+        <motion.div
         variants={fadeIn('up', 0.3, 0.3)}
         initial="hidden"
         whileInView="show"
         viewport={{ once: true, amount: 0.3 }}  
-        onClick={() => navigate('/contact')} className='bg-black text-white py-3 px-5 rounded-full w-60 hover:bg-zinc-600 ease-in duration-300 '>
-          CLICK HERE
-        </motion.button>
+        className='flex justify-center text-center mx-auto mb-10 text-md text-red-700 font-serif font-semibold '>
+          <p>As a leading mobile app development company, we help ambitious entrepreneurs disrupt the ordinary,
+             set new benchmarks, and drive innovation through full-fledged mobile app development services.</p>
+        </motion.div>
         
+        </div>
+
+        <div className='flex-row justify-center grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-4 items-center'>
+          <motion.div
+        variants={fadeIn('up', 0.3, 0.3)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.3 }}  
+        className='flex-col justify-center text-center mx-auto text-red-700 font-serif font-semibold '>
+          <h1>1,200+</h1> <p className='text-sm font-prompt'>Mobile apps built for diverse industries</p>
+        </motion.div>
+
+        <motion.div
+        variants={fadeIn('up', 0.3, 0.3)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.3 }}  
+        className='flex-col justify-center text-center mx-auto text-red-700 font-serif font-semibold '>
+          <h1>$500M+</h1> <p className='text-sm font-prompt'>Raised by clients through funding rounds</p>
+        </motion.div>
+
+        <motion.div
+        variants={fadeIn('up', 0.3, 0.3)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.3 }}  
+        className='flex-col justify-center text-center mx-auto text-red-700 font-serif font-semibold '>
+          <h1>12M+</h1> <p className='text-sm font-prompt'>Mobile apps built for diverse industries</p>
+        </motion.div>
+
+        <motion.div
+        variants={fadeIn('up', 0.3, 0.3)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.3 }}  
+        className='flex-col justify-center text-center mx-auto text-red-700 font-serif font-semibold '>
+          <h1>50+</h1> <p className='text-sm font-prompt'>5-Star Reviews on Clutch for Mobile App Development Services</p>
+        </motion.div>
         </div>
         
         </div>
